eb9fce5f5859d2321a8ad3862dabc76a7cc8da2f
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2024-09-10 18:04:14 +0300

    Вынес код обработки файла из main в processFile. Отформатировал код

--- LABS/LABS/LAB1_TASK2.cpp
+++ LABS/LABS/LAB1_TASK2.cpp
@@ -14,109 +14,116 @@ const int INPUT_FILE_OPEN_ERROR = 2;
 const int OUTPUT_FILE_OPEN_ERROR = 3;
 
 void reverseWord(char* word);
+void processFile(std::ifstream& input, std::ofstream& output);
+
 void showErrorMessage(int code);
 
 int main(int argc, char* argv[])
 {
-    char ch = 0;
-    int i = 0;
-    char word[255]{};
+	std::ifstream inFile;
+	std::ofstream outFile;
 
-    std::ifstream inFile;
-    std::ofstream outFile;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
 
-    setlocale(LC_ALL, "Russian");
+	if (argc <= 2)
+	{
+		showErrorMessage(ARGUMENTS_COUNT_ERROR);
+		return EXIT_FAILURE;
+	}
 
-    if (argc <= 2)
-    {
-        showErrorMessage(ARGUMENTS_COUNT_ERROR);
-        return EXIT_FAILURE;
-    }
+	inFile.open(argv[1]);
+	if (inFile.is_open() == false)
+	{
+		showErrorMessage(INPUT_FILE_OPEN_ERROR);
+		return EXIT_FAILURE;
+	}
 
-    inFile.open(argv[1]);
-    if (inFile.is_open() == false)
-    {
-        showErrorMessage(INPUT_FILE_OPEN_ERROR);
-        return EXIT_FAILURE;
-    }
+	outFile.open(argv[2]);
+	if (outFile.is_open() == false)
+	{
+		showErrorMessage(OUTPUT_FILE_OPEN_ERROR);
+		return EXIT_FAILURE;
+	}
 
-    outFile.open(argv[2]);
-    if (outFile.is_open() == false)
-    {
-        showErrorMessage(OUTPUT_FILE_OPEN_ERROR);
-        return EXIT_FAILURE;
-    }
+	processFile(inFile, outFile);
 
-    while (inFile.get(ch))
-    {
-        if (ch == ' ' || ch == '\n')
-        {
-            if (i > 0)
-            {
-                word[i] = '\0';
-                reverseWord(word);
-                outFile << word;
-                i = 0;
-            }
-            outFile << ch;
-        }
-        else
-        {
-            if (i < 255)
-            {
-                word[i++] = ch;
-            }
-        }
-    }
+	inFile.close();
+	outFile.close();
 
-    if (i > 0) {
-        word[i] = '\0';
-        reverseWord(word);
-        outFile << word;
-    }
+	system(("start " + std::string(argv[2])).c_str());
 
-    inFile.close();
-    outFile.close();
-
-    system(("start " + std::string(argv[2])).c_str());
-
-    cout << "Программа успешно завершила работу" << endl;
-    return EXIT_SUCCESS;
+	cout << "Программа успешно завершила работу" << endl;
+	return EXIT_SUCCESS;
 }
 
 void reverseWord(char* word)
 {
-    int len = strlen(word);
-    for (int i = 0; i < len / 2; ++i) {
-        char temp = word[i];
-        word[i] = word[len - i - 1];
-        word[len - i - 1] = temp;
-    }
+	int len = strlen(word);
+	for (int i = 0; i < len / 2; ++i) {
+		char temp = word[i];
+		word[i] = word[len - i - 1];
+		word[len - i - 1] = temp;
+	}
+}
+
+void processFile(std::ifstream& input, std::ofstream& output)
+{
+	char ch = 0;
+	int i = 0;
+	char word[255]{};
+
+	while (input.get(ch))
+	{
+		if (ch == ' ' || ch == '\n')
+		{
+			if (i > 0)
+			{
+				word[i] = '\0';
+				reverseWord(word);
+				output << word;
+				i = 0;
+			}
+			output << ch;
+		}
+		else
+		{
+			if (i < 255)
+			{
+				word[i++] = ch;
+			}
+		}
+	}
+
+	if (i > 0) {
+		word[i] = '\0';
+		reverseWord(word);
+		output << word;
+	}
 }
 
 void showErrorMessage(int code)
 {
-    switch (code)
-    {
-    case ARGUMENTS_COUNT_ERROR:
-        cout << "Передано недостаточное количество параметров" << endl;
-        cout << "После названия программы через пробел должен быть указан путь входного файла" << endl;
-        cout << "После него через пробел должен быть указан путь выходного файла" << endl;
-        break;
-    case INPUT_FILE_OPEN_ERROR:
-        cout << "Входной файл не существует или находится в другом месте" << endl;
-        break;
-    case OUTPUT_FILE_OPEN_ERROR:
-        cout << "Возникли проблемы с созданием выходного файла" << endl;
-        break;
-    default:
-        cout << "Ошибки с таким кодом не существует" << endl;
-        return;  
-    }
-    cout << endl << "Программа завершила работу с ошибкой" << endl;
+	switch (code)
+	{
+	case ARGUMENTS_COUNT_ERROR:
+		cout << "Передано недостаточное количество параметров" << endl;
+		cout << "После названия программы через пробел должен быть указан путь входного файла" << endl;
+		cout << "После него через пробел должен быть указан путь выходного файла" << endl;
+		break;
+	case INPUT_FILE_OPEN_ERROR:
+		cout << "Входной файл не существует или находится в другом месте" << endl;
+		break;
+	case OUTPUT_FILE_OPEN_ERROR:
+		cout << "Возникли проблемы с созданием выходного файла" << endl;
+		break;
+	default:
+		cout << "Ошибки с таким кодом не существует" << endl;
+		return;
+	}
+	cout << endl << "Программа завершила работу с ошибкой" << endl;
 }
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
